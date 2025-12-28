007a88496a531ee3813203146976ae0fa04aaa61
mateus-fpl <mateus_paes@yahoo.com.br>
2025-12-28 18:24:19 -0300

    Exercícios sequenciais em Python

--- Python/media_10.py
+++ Python/media_10.py
@@ -5,5 +5,6 @@ for i in range(num):
     valor_digitado = float(input(f"Digite o {i + 1}º valor: "))
     soma += valor_digitado
 
-media = soma/num
+if num > 0:
+    media = soma/num
 print(f"O resultado final é {soma} e a média dos valores é {media}")
