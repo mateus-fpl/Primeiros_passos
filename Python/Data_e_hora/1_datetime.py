ef2763e7ba1e7a994527f78fef92fef02dead868
mateus-fpl <mateus_paes@yahoo.com.br>
2026-01-30 07:20:16 -0300

    Estudos com datas e horas

--- Python/Data_e_hora/1_datetime.py
+++ Python/Data_e_hora/1_datetime.py
@@ -1,4 +1,13 @@
-from datetime import date
+from datetime import date, datetime, time
 
-data = date(2025, 1,29)
+data = date(2024, 6,19)
 print(data)
+
+print(date.today())
+
+data_hora = datetime(2024, 6,19, 22,4,59)
+print(data_hora)
+print(datetime.today())
+
+hora = time(10, 20 ,0)
+print(hora)
