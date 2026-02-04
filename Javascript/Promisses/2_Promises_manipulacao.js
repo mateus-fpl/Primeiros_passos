cfe861cff336b1452c5ace8afd2078475b2cab26
mateus-fpl <mateus_paes@yahoo.com.br>
2026-02-04 13:54:52 -0300

    Último exercício

--- Javascript/Promisses/2_Promises_manipulacao.js
+++ Javascript/Promisses/2_Promises_manipulacao.js
@@ -4,17 +4,30 @@ const path = require('path')
 
 const filePath = path.resolve(__dirname, 'tarefas.csv')
 
-const promessaDaLeituraDoArquivo = fs.promises.readFile('tarefas.csv')
 
-promessaDaLeituraDoArquivo
-    .then((arquivo) => arquivo.toString('utf8'))
-    .then((textoDoArquivo) => textoDoArquivo.split('\n').slice(1))
-    .then((linhasSemOCabecalho) => linhasSemOCabecalho.map((linha) => {
-        const [nome, feito] = linha.split(';')
-        return {
-            nome,
-            feito: feito.trim() === 'true'
-        }
-    }))
-    .then((listaDeTarefas) => console.log(listaDeTarefas))
-    .catch((error)=> console.log('Deu ruim!', error))
+async function buscarArquivo(){
+    try{
+    const arquivo = await fs.promises.readFile(filePath)
+    const textoDoArquivo = arquivo.toString('utf8')
+    console.log(textoDoArquivo)
+    } catch(error){
+        console.log(error)
+    } finally {
+       console.log('finalizou!')
+    }
+}
+
+buscarArquivo()
+
+// promessaDaLeituraDoArquivo
+//     .then((arquivo) => arquivo.toString('utf8'))
+//     .then((textoDoArquivo) => textoDoArquivo.split('\n').slice(1))
+//     .then((linhasSemOCabecalho) => linhasSemOCabecalho.map((linha) => {
+//         const [nome, feito] = linha.split(';')
+//         return {
+//             nome,
+//             feito: feito.trim() === 'true'
+//         }
+//     }))
+//     .then((listaDeTarefas) => console.log(listaDeTarefas))
+//     .catch((error)=> console.log('Deu ruim!', error))
