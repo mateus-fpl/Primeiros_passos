50116d0ff155e9c8ae5c4e292363b9a9127c67e1
Mateus Paes <mateus_paes@yahoo.com.br>
2026-02-21 19:53:46 -0300

    Add files via upload

---
+++ SQL/Scripts/company.sql
@@ -0,0 +1,77 @@
+create schema if not exists company_constraints;
+use company_constraints;
+
+-- restrição atribuída a um documento --
+-- create domain D_num as int check(D_num> 0 and D_num<21);--
+
+create table if not exists employee(
+	Fname varchar(15) not null,
+	Minit char,
+	lname varchar(15) not null,
+	Ssn char(9) not null,
+	Bdate date,
+	Address varchar(30),
+	sex char,
+	Salary decimal(10,2),
+	Super_ssn char(9),
+	Dno int not null,
+    constraint chk_salary_employee check (Salary > 2000.00),
+    constraint pk_employee primary key (Ssn)
+);
+
+desc employee; 
+
+create table if not exists departament (
+	Dname varchar(15) not null,
+    Dnumber int not null,
+    Mgr_ssn char(9),
+    Mgr_start_date date,
+    Dept_create_date date,
+    constraint chk_date_dept check (Dept_create_date < Mgr_start_date),
+    constraint pk_dept primary key (Dnumber),
+    constraint unique_name_dept unique (Dname),
+    foreign key (Mgr_ssn) references employee(Ssn)
+);
+
+create table if not exists dept_locations(
+	Dnumber int not null,
+    Dlocation varchar(15) not null,
+    constraint pk_dept_locations primary key (Dnumber, Dlocation),
+    constraint fk_dept_locations foreign key (Dnumber) references departament (Dnumber)
+);
+
+create table if not exists project(
+	Pname varchar(15) not null,
+    Pnumber int not null,
+    Plocation varchar(15),
+    Dnum int not null,
+    primary key (Pnumber),
+    constraint unique_project unique (Pname),
+    constraint fk_project foreign key (Dnum) references departament(Dnumber)
+);
+
+create table if not exists works_on(
+	Essn char(9) not null,
+    Pno int not null,
+    Hours decimal(3,1) not null,
+    primary key (Essn, Pno),
+    constraint fk_employee_works_on foreign key (Essn) references employee(Ssn),
+    constraint fk_project_works_on foreign key (Pno) references project(Pnumber)
+);
+drop table dependent;
+create table if not exists dependent (
+	Essn char(9) not null,
+    Dependent_name varchar(15) not null,
+    Sex char, -- F ou M
+    Bdate date,
+    Relationship varchar(8),
+    primary key (Essn, Dependent_name),
+    constraint fk_dependent foreign key (Essn) references employee (Ssn)
+);
+
+show tables;
+desc works_on;
+
+select * from information_schema.table_constraints 
+where constraint_schema = 'company_constraints'
+
