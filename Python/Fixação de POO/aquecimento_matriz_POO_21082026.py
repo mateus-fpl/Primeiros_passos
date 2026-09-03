efc9515fdf6a5b34dbb8db89b067da0cfd7ee67a
mateus-fpl <mateus_paes@yahoo.com.br>
2026-09-03 06:48:12 -0300

    Novos treinos

--- Python/Fixação de POO/aquecimento_matriz_POO_21082026.py
+++ Python/Fixação de POO/aquecimento_matriz_POO_21082026.py
@@ -1,39 +1,39 @@
-# ### 🤺 Desafio: Colisão de Arenas Pokémon
+pokedex = []
+hp_pokemon = []
+ataque_pokemon = []
 
-# Imagine um jogo onde duas arenas $3 \times 3$ se cruzam no campo de batalha:
-
-# * **Matriz 1 (Arena 1):** Guarda os **nomes/espécies** dos Pokémon em cada coordenada.
-# * **Matriz 2 (Arena 2):** Guarda o **nível (Level/Power)** dos Pokémon nas mesmas coordenadas exactas.
-
-# #### O que você deve construir:
-
-# 1. **Estrutura de Matrizes ($3 \times 3$):**
-# * Monte a **Matriz 1** com 9 Pokémon de sua escolha (ex: `[0][0]` é o Pikachu, `[0][1]` é o Charizard, etc.).
-# * Monte a **Matriz 2** com os níveis de cada um (números de 1 a 100) exatamente nas mesmas posições.
-
-
-# 2. **Classes (POO com Polimorfismo):**
-# * **Classe Pai `Pokemon`:**
-# * Atributos: `nome` e `nivel`.
-# * Método `atacar()`: Exibe algo genérico como `"Pokémon X (Nível Y) lança um ataque básico!"`.
-
-
-# * **Classe Filha `Eletrico` (Herda de `Pokemon`):**
-# * Sobrescreva `atacar()`: `"Pokémon X (Nível Y) solta um Choque do Trovão!"`.
+for i in range (2):
+    linha_pokedex = []
+    linha_hp = []
+    linha_ataque = []
+    for j in range (2):
+        pokemon = input(f"Digite o seu pokemon: ")
+        hp_bichinho = int(input(f"Digite o HP do {pokemon}: "))
+        poder_de_ataque = int(input(f"Digite o poder de ataque do {pokemon}: "))
+        linha_pokedex.append(pokemon)
+        linha_hp.append(hp_bichinho)
+        linha_ataque.append(poder_de_ataque)
+    pokedex.append(linha_pokedex)
+    hp_pokemon.append(linha_hp)
+    ataque_pokemon.append(linha_ataque)
+    print(f"{pokemon} possui {hp_bichinho} HP com ataque de {poder_de_ataque} pontos.")
 
 
-# * **Classe Filha `Fogo` (Herda de `Pokemon`):**
-# * Sobrescreva `atacar()`: `"Pokémon X (Nível Y) lança uma Lança-Chamas!"`.
+class Pokemon:
+    def __init__(self, nome, hp, ataque):
+        self.nome = nome
+        self.hp = hp
+        self.ataque = ataque
 
+    def atacar(self, alvo):
+        print(f"{self.nome} atacou {alvo.nome}")
 
+    def receber_dano(self,alvo):
+        self.hp = self.hp - alvo.ataque
+        print(f"{self.nome} recebeu um ataque de {alvo.nome} e perdeu {alvo.ataque} de HP")
+        print(f"HP restantes: {self.hp}")
 
-
-# 3. **A Mecânica de Cruzar as Matrizes:**
-# * Faça um `for` duplo (`i` para linha, `j` para coluna) para varrer as duas matrizes **ao mesmo tempo**.
-# * Em cada célula `[i][j]`, pegue o nome da Matriz 1 e o nível da Matriz 2.
-# * Instancie a classe correspondente ao tipo dele (se for elétrico, crie um `Eletrico`, se for fogo, crie um `Fogo`).
-# * Adicione esse objeto instanciado em uma **lista de combate**.
-
-
-# 4. **O Duelo (O Grand Finale):**
-# * Faça um `for` que percorre essa lista de combate fazendo os Pokémon duelarem em duplas (o índice 0 contra o índice 1, o 2 contra o 3, etc.) usando o método `.atacar()` de cada um!
+poke1 = Pokemon(pokedex[0][0],hp_pokemon[0][0],ataque_pokemon[0][0])
+poke2 = Pokemon(pokedex[0][0],hp_pokemon[0][0],ataque_pokemon[0][0])
+poke1.atacar(poke2)
+poke2.receber_dano(poke1)
